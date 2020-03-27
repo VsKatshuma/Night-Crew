dd709f4d5303db0a005e3b8f9469c09ca706bd3a
Ketoni <juhanakekoni@yahoo.com>
2020-03-27 03:49:40 +0200

    Add weapon class

---
+++ weapon.js
@@ -0,0 +1,34 @@
+
+class Weapon {
+    constructor(projectile, amount, spread, rate) {
+        this.projectile = projectile;
+        this.amount = amount;
+        this.spread = spread;
+        this.rate = rate;
+
+        this.fireTime = 0;
+        this.ready = true;
+    }
+
+    shoot(now, direction) {
+        // TODO amount, spread
+
+        this.fireTime = now;
+
+        // Populate a copy of the weapon's projectile
+        var proj = this.projectile.clone();
+        proj.speed.x *= direction.x;
+        proj.speed.y *= direction.y;
+
+        this.ready = false;
+        return proj;
+    }
+
+    load(now) {
+        if ((now - this.fireTime) > this.rate) {
+            this.ready = true;
+        }
+    }
+
+}
+
