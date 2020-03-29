7702e3f2d45482794ceff499ebf43371154bdb23
Ketoni <juhanakekoni@yahoo.com>
2020-03-29 12:25:41 +0300

    add msToTime helper function

--- engine.js
+++ engine.js
@@ -104,6 +104,23 @@ var gameObjects = {
     playerProjectiles: []
 };
 
+function msToTime(s) {
+	// Pad to 2 or 3 digits, default is 2
+	function pad(n, z) {
+		z = z || 2;
+		return ('00' + n).slice(-z);
+	}
+
+	var ms = s % 1000;
+	s = (s - ms) / 1000;
+	var secs = s % 60;
+	s = (s - secs) / 60;
+	var mins = s % 60;
+	var hrs = (s - mins) / 60;
+
+	return pad(hrs) + ':' + pad(mins) + ':' + pad(secs)
+}
+
 // Initialize game state and play time
 var gameState = null; // 0 = title, 1 = game, 2 = failure state
 var startTime = NaN;
